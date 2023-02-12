d8c1bf58d0ee94c12a23da7647432d541a2ca1df
Victor Shyba <victor.shyba@gmail.com>
2023-02-11 23:17:08 -0300

    bench check and insert

--- benches/simple_benchmark.rs
+++ benches/simple_benchmark.rs
@@ -3,7 +3,8 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let filter: AtomicFilter<480, 43> = AtomicFilter::default();
-    c.bench_function("batata", |b| b.iter(|| filter.insert(&black_box("batata"))));
+    c.bench_function("insert", |b| b.iter(|| filter.insert(&black_box("insert"))));
+    c.bench_function("check", |b| b.iter(|| filter.check(&black_box("check"))));
 }
 
 criterion_group!(benches, criterion_benchmark);
